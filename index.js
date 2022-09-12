7d753bc568c42664628b64077a80809dddae1126
Daniel Garden <daniel@embarkagency.com.au>
2022-09-12 14:58:12 +1000

    logs

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-import chalk from 'chalk';
+// import chalk from 'chalk';
 import cors from 'cors';
 import express from 'express';
 const app = express();
+
+import http from 'http';
+import { Server } from "socket.io";
+const server = http.createServer(app);
+const io = new Server(server);
+
+const log = [];
+const blacklist = ['/favicon.ico', '/robots.txt', '/log', '/'];
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/', express.static('public'));
+
+app.get('/log', (req, res) => {
+    res.json(log);
+});
+
+io.on('connection', (socket) => {
+    socket.emit('log-all', log);
+});
 
 app.use((req, res, next) => {
     const data = {
@@ -14,15 +32,25 @@ app.use((req, res, next) => {
         method: req.method,
         query: req.query,
         body: req.body,
-        time: new Date().toLocaleString()
+        time: Date.now()
     };
-    console.log('\n', chalk.blue(data.time), chalk.green(data.method), chalk.yellow(data.path));
-    console.log(data.body && data.body.return ? data.body.return : {
-        ...data.query,
-        ...data.body,
-    });
-    res.json(data.body && data.body.return ? data.body.return : data);
+
+    if(!blacklist.includes(req.path)) {
+        log.push(data);
+        if(log.length > 100) {
+            log.shift();
+        }
+        io.emit('log-new', data);
+    }
+
+
+    res.json({
+        status: 'ok',
+        data
+    })
     return;
 });
 
-app.listen(port, console.log(`Mock server is running on port ${port}`));
+server.listen(port, () => {
+    console.log(`Mock server is running on port ${port}`)
+});
